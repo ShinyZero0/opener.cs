291d0bdd553020c1770b6a6c7f338872f84ef05e
ShinyZero0 <shinyzero0@gmail.com>
2023-04-15 20:21:30 +0300

    rebranding; mimes => filename templates

--- Program.cs
+++ Program.cs
@@ -3,71 +3,70 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
-// using Newtonsoft.Json;
-using MimeKit;
 
 internal static class Program
 {
-    private static void Main(string[] args)
+    private static int Main(string[] args)
     {
         string configFile = Path.Combine(
-            Environment.GetEnvironmentVariable("HOME"),
-            ".config/jajaro.json"
+            Environment.GetEnvironmentVariable("HOME")!,
+            ".config/associations.json"
         );
         string raw;
         try
         {
             raw = File.ReadAllText(configFile);
-        }
-        catch
-        {
-            Console.WriteLine("Create a config file in ~/.config/jajaro.json please");
-            return;
-        }
-        try
-        {
-            // Config = JsonConvert.DeserializeObject<ConfigFile>(raw);
-            Config = JsonSerializer.Deserialize<ConfigFile>(raw, SourceGenerationContext.Default.ConfigFile);
+            Config = JsonSerializer.Deserialize<ConfigFile>(
+                raw,
+                SourceGenerationContext.Default.ConfigFile
+            );
         }
         catch (Exception exc)
         {
             Console.WriteLine($"Problem with your config:\n{exc}");
+            return 1;
         }
-        foreach (Handler handler in Config.Handlers)
+
+        foreach (Handler handler in Config.Handlers.Reverse())
         {
-            foreach (string mimetype in handler.Types)
+            foreach (string regex in handler.Types)
             {
-                HandlerMaps[mimetype] = handler;
+                RegexHandlers[regex] = handler;
             }
         }
-        foreach (string file in args)
-        {
-            OpenFile(file);
-        }
-        return;
+        OpenFiles(args);
+        return 0;
     }
 
-    private static Dictionary<string, Handler> HandlerMaps = new();
+    private static Dictionary<string, Handler> RegexHandlers = new();
     private static ConfigFile Config;
 
-    private static void OpenFile(string fileName)
+    private static void OpenFiles(string[] fileNames)
     {
-        Handler? handler = null;
-        foreach (KeyValuePair<string, Handler> entry in HandlerMaps.Reverse())
+        Tuple<string, Handler>? handlerTuple = null;
+        foreach (KeyValuePair<string, Handler> entry in RegexHandlers)
         {
-            if (Regex.IsMatch(MimeTypes.GetMimeType(fileName), entry.Key))
+            if (Regex.IsMatch(fileNames[0], entry.Key))
             {
-                handler = entry.Value;
+                handlerTuple = new(entry.Key, entry.Value);
                 break;
             }
         }
-        if (handler is null)
+        if (handlerTuple is null)
         {
             Console.WriteLine("handler not defined");
             return;
         }
+        foreach (string fileName in fileNames)
+        {
+            if (!Regex.IsMatch(fileName, handlerTuple.Item1))
+            {
+                Console.WriteLine("These files have different handlers!");
+                return;
+            }
+        }
         Process proc;
-        if (Console.IsInputRedirected && handler.Term)
+        if (Console.IsInputRedirected && handlerTuple.Item2.Term)
         {
             string termPrefix = Config.Term;
             proc = new()
@@ -75,7 +74,14 @@ internal static class Program
                 StartInfo = new ProcessStartInfo()
                 {
                     FileName = "/bin/sh",
-                    Arguments = String.Join(' ', "-c \"", termPrefix, handler.Exec, fileName, "\""),
+                    Arguments = String.Join(
+                        ' ',
+                        "-c \"",
+                        termPrefix,
+                        handlerTuple.Item2.Exec,
+                        string.Join(' ', fileNames),
+                        "\""
+                    ),
                     CreateNoWindow = false,
                 }
             };
@@ -87,7 +93,13 @@ internal static class Program
                 StartInfo = new ProcessStartInfo()
                 {
                     FileName = "/bin/sh",
-                    Arguments = String.Join(' ', "-c \"", handler.Exec, fileName, "\""),
+                    Arguments = String.Join(
+                        ' ',
+                        "-c \"",
+                        handlerTuple.Item2.Exec,
+                        string.Join(' ', fileNames),
+                        "\""
+                    ),
                 }
             };
         }
@@ -108,12 +120,10 @@ public partial class Handler
     [JsonInclude]
     public bool Term = false;
 
-    // public string TermExec;
-
     public Handler() { }
 }
 
-public struct ConfigFile
+public class ConfigFile
 {
     [JsonInclude]
     public string Term;
@@ -121,9 +131,9 @@ public struct ConfigFile
     [JsonInclude]
     public Handler[] Handlers;
 }
+
+// some magic for AOT
 [JsonSourceGenerationOptions(WriteIndented = true)]
 [JsonSerializable(typeof(ConfigFile))]
 [JsonSerializable(typeof(Handler))]
-internal partial class SourceGenerationContext : JsonSerializerContext
-{
-}
+internal partial class SourceGenerationContext : JsonSerializerContext { }
