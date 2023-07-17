58e6d7b3949c7130e8363e39ca36148cb413896d
zero@void <shinyzero0@gmail.com>
2023-07-17 03:02:44 +0300

    add nix flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,44 @@
 {
-  description = "A very basic flake";
-
+  description = "A very basic .NET flake";
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
-  outputs = { self, nixpkgs, }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      dotnetPkgs = with pkgs;
+        (with dotnetCorePackages; combinePackages [ sdk_6_0 sdk_7_0 ]);
+      deps = with pkgs; [ clang zlib zlib.dev icu pkg-config ];
     in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        name = "opener.cs";
-        buildInputs = [ pkgs.dotnet-sdk_7 pkgs.clang pkgs.zlib ];
+
+      devShells.${system}.default = pkgs.mkShell {
+        name = "<name>";
+        buildInputs = with pkgs; [ dotnetPkgs omnisharp-roslyn ];
+        shellHook = ''
+          export DOTNET_ROOT=${dotnetPkgs}
+        '';
       };
-      packages.system.default = nixpkgs.stdenv.mkDerivation {
-        
+
+      apps.${system}.fetch-deps = {
+        type = "app";
+        program = "${self.packages.${system}.default.passthru.fetch-deps}";
       };
+
+      packages.${system}.default = with pkgs;
+        buildDotnetModule {
+          pname = "<name>";
+          version = "0.0.1";
+          src = ./.;
+          nugetDeps = ./deps.nix;
+          dotnet-sdk = dotnetCorePackages.sdk_7_0;
+          dotnet-runtime = dotnetCorePackages.runtime_7_0;
+          buildInputs = deps;
+          nativeBuildInputs = [ ] ++ deps;
+          selfContainedBuild = true;
+
+          DOTNET_ROOT = "${dotnetPkgs}";
+          LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+          NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([ stdenv.cc.cc ] ++ deps);
+          NIX_LD = "${stdenv.cc.libc_bin}/bin/ld.so";
+        };
     };
 }
